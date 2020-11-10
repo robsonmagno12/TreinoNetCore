407370e2bd41c38995dc51953cbe40392a7ae724
Robson Magno <robsonmagno2@hotmail.com>
2020-11-09 22:50:26 -0300

    treino pegado dotnet

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TreinoUdemy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
